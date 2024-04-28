8eeb63db506f09ccae09463f0b09144a26dcc933
Roman_M <prominb@gmail.com>
2024-04-29 00:08:10 +0300

    imp-ed get_author func and save to file e012

--- bs4_main.py
+++ bs4_main.py
@@ -6,31 +6,61 @@ from bs4 import BeautifulSoup
 
 
 BASE_URL = "https://quotes.toscrape.com"
-# BASE_URL = "https://quotes.toscrape.com/page/10/"
-FILE_QUOTES = "quotes2.json"
-FILE_AUTHORS = "authors2.json"
+FILE_QUOTES = "quotes.json"
+FILE_AUTHORS = "authors.json"
 
 
 def start_parser():
-    quotes_list = list()
+    # quotes_list = list()
     authors_list = list()
-    response = requests.get(BASE_URL)
-    soup = BeautifulSoup(response.text, 'lxml')
-    quotes_on_page = get_quotes_on_page(soup)
-    quotes_list.extend(quotes_on_page)
-    authors_links_list = get_links_authors(soup)
-    authors_on_page = get_authors_on_page(authors_links_list)
-    authors_list.extend(authors_on_page)
-    next_page = get_next_page(BASE_URL)
-    while next_page is not None:
-        print(next_page)
-        sleep(1.1)
-        next_page = get_next_page(next_page)
-    else:
-        print("THE END!")
+    # print(authors_list)
+    # print(f"===>>> START PAGE {BASE_URL}")
+    # response = requests.get(BASE_URL)
+    # soup = BeautifulSoup(response.text, 'lxml')
+    # quotes_on_page = get_quotes_on_page(soup)
+    # quotes_list.extend(quotes_on_page)
+    # authors_link_list = get_links_authors(soup, authors_list)
+    # next_page = get_next_page(BASE_URL)
+    # while next_page is not None:
+    #     sleep(0.5)
+    #     print(f"===>>> START PAGE {next_page}")
+    #     next_response = requests.get(next_page)
+    #     next_soup = BeautifulSoup(next_response.text, 'lxml')
+    #     quotes_on_page = get_quotes_on_page(next_soup)
+    #     quotes_list.extend(quotes_on_page)
+    #     authors_link_list = get_links_authors(next_soup, authors_link_list)
+    #     sleep(0.5)
+    #     next_page = get_next_page(next_page)
+    # else:
+    #     print("THE END!")
+
+    # with open("authors_link_list.txt", "w", encoding="utf-8") as fh:
+    #     for link_elem in authors_link_list:
+    #         fh.write(link_elem + "\n")
+    with open("authors_link_list.txt") as fh:
+        lines = fh.readlines()
+        # print(lines)
+    new_link_list = list()
+    for link in lines:
+        new_link = link.rstrip("\n")
+        # print(f"NNN{test123}BBBB")
+        new_link_list.append(new_link)
+    # print(len(new_link_list))
+    # for idx in range(15):
+    for lnk in new_link_list:
+        # author_from_page = get_author(new_link_list[idx])
+        author_from_page = get_author(lnk)
+        authors_list.append(author_from_page)
+    
+    # for link_elem in authors_link_list:
+    #     print(link_elem)
+    #     author_from_page = get_author(link_elem)
+    #     authors_list.append(author_from_page)
+    # print(authors_list)
+    
 
     # print(write_to_json(FILE_QUOTES, quotes_list))
-    # print(write_to_json(FILE_AUTHORS, authors_list))
+    print(write_to_json(FILE_AUTHORS, authors_list))
 
 
 def get_quotes_on_page(soup: BeautifulSoup) -> list[dict]:
@@ -49,37 +79,30 @@ def get_quotes_on_page(soup: BeautifulSoup) -> list[dict]:
     return result_list
 
 
-def get_authors_on_page(links_list: list) -> list[dict]:
-    if links_list:
-        result_list = list()
-        for link in links_list:
-            sleep(1.2)
-            response = requests.get(link)
-            soup = BeautifulSoup(response.text, 'lxml')
-            fullname = soup.find('h3', class_="author-title").text
-            born_date = soup.find('span', class_="author-born-date").text
-            born_location = soup.find('span', class_="author-born-location").text
-            description = soup.find('div', class_="author-description").text.strip()
-            result_list.append(
-                {
-                    "fullname": fullname,
-                    "born_date": born_date,
-                    "born_location": born_location,
-                    "description": description
-                }
-            )
-        return result_list
-    else:
-        return ["Authors links List is Empty!"]
+def get_author(link: str) -> list[dict]:
+    sleep(1.2)
+    print(f"===>>> START {link}")
+    response = requests.get(link)
+    soup = BeautifulSoup(response.text, 'lxml')
+    fullname = soup.find('h3', class_="author-title").text
+    born_date = soup.find('span', class_="author-born-date").text
+    born_location = soup.find('span', class_="author-born-location").text
+    description = soup.find('div', class_="author-description").text.strip()
+    result = {
+        "fullname": fullname,
+        "born_date": born_date,
+        "born_location": born_location,
+        "description": description
+    }
+    return result
 
 
-def get_links_authors(soup: BeautifulSoup) -> list:
-    author_links_list = list()
+def get_links_authors(soup: BeautifulSoup, author_link_list: list) -> list:
     get_author_links = soup.find_all('a', string="(about)")
     for lk in get_author_links:
-        if (BASE_URL + lk["href"]) not in author_links_list:
-            author_links_list.append(BASE_URL + lk["href"])
-    return author_links_list
+        if (BASE_URL + lk["href"]) not in author_link_list:
+            author_link_list.append(BASE_URL + lk["href"])
+    return author_link_list
 
 
 def write_to_json(filename, data):
