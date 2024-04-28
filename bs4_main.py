7f66075fad1902aab3b907c3ebe07b09c88b427a
Roman_M <prominb@gmail.com>
2024-04-28 15:24:32 +0300

    impl-ted 1st page find and save to json

--- bs4_main.py
+++ bs4_main.py
@@ -8,6 +8,7 @@ BASE_URL = "https://quotes.toscrape.com"
 
 
 def get_quotes():
+    json_list = list()
     response = requests.get(BASE_URL)
     soup = BeautifulSoup(response.text, 'lxml')
     # quotes = soup.find_all('span', class_='text')
@@ -21,12 +22,30 @@ def get_quotes():
     # for author in authors_list:
     #     print(author)
     tags_list = [tag.text.replace('Tags:', '').strip().split('\n') for tag in soup.find_all('div', class_="tags")]
-    for tags in tags_list:
-        print(tags)
+    # for tags in tags_list:
+    #     print(tags)
+    # json_list = li
+    for quote, author, tags in zip(quotes_list, authors_list, tags_list):
+        # print(author, quote, tags)
+        json_list.append(
+            {
+                "tags": tags,
+                "author": author,
+                "quote": quote
+            }
+        )
+    # print(json_list)
+    print(write_to_json("quotes.json", json_list))
 
 
 def get_authors():
     pass
+
+
+def write_to_json(filename, json_list):
+    with open(filename, "w", encoding="utf-8") as fh:
+        json.dump(json_list, fh, indent=4)
+    return "DONE."
 
 
 def main():
